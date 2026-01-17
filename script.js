31cab472c6a73055aa36181dd9de157dc8f29310
Vaibhav Sharma <92776886+Vaibhav992@users.noreply.github.com>
2026-01-17 19:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+// Focus first input on load
+codes[0].focus();
+
+codes.forEach((code, index) => {
+  code.addEventListener('keydown', (e) => {
+
+    // Allow only numbers and backspace
+    if (e.key >= '0' && e.key <= '9') {
+      code.value = '';
+      setTimeout(() => {
+        if (index < codes.length - 1) {
+          codes[index + 1].focus();
+        }
+      }, 10);
+    }
+
+    if (e.key === 'Backspace') {
+      if (code.value === '' && index > 0) {
+        codes[index - 1].focus();
+        codes[index - 1].value = '';
+      }
+    }
+  });
+});
